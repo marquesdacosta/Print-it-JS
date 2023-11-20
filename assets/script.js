a96b84d32ebdb386132080efb3d86f9f84bc3de7
Thomas Marques <tomasmarquesdacosta@gmail.com>
2023-11-20 19:46:24 +0100

    premier commit avec projet js terminé

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,54 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+
+let flecheGauche = document.getElementById('fleche-gauche');
+let flecheDroite = document.getElementById('fleche-droite');
+
+let imgActuel = document.querySelector('.banner-img');
+let tagLine = document.getElementById('tag-line');
+let dots = document.querySelectorAll('.dots .dot');
+let index = 0;
+
+
+function bulletPoint(){
+
+	imgActuel.src = './assets/images/slideshow/' + slides[index].image;
+
+	tagLine.innerHTML = slides[index].tagLine;
+	
+	for (let i = 0; i < dots.length; i++) {
+    dots[i].classList.remove('dot_selected');
+}	
+	dots[index].classList.add('dot_selected');
+}
+
+
+
+flecheGauche.addEventListener("click", function(){
+	console.log("j'ai cliqué sur la fleche gauche");
+
+
+	 if (index === 0) {
+        index = slides.length - 1;
+    } else {
+        index--;
+    }
+
+	bulletPoint();
+})
+
+flecheDroite.addEventListener("click", function(){
+	console.log("j'ai cliqué sur la fleche droite");
+
+
+	 if (index === slides.length - 1) {
+        index = 0;
+    } else {
+        index++;
+    }
+
+	bulletPoint();
+})
+
